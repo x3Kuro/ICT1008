18d795e2432bcecdf427c73c221f465735978518
J3n3ns <stoneisfun@gmail.com>
2020-04-02 22:18:05 +0800

    osmid generator?

---
+++ ICT1008_Project/abc.py
@@ -0,0 +1,126 @@
+import networkx as nx
+import osmnx as ox
+import geopandas as gpd2
+import pandas
+import numpy as np
+
+def get_node(element):
+    """
+    Convert an OSM node element into the format for a networkx node.
+    Parameters
+    ----------
+    element : dict
+        an OSM node element
+    Returns
+    -------
+    dict
+    """
+    useful_tags_node = ['ref', 'highway', 'route_ref', 'name', 'asset_ref']
+    node = {}
+    node['y'] = element['lat']
+    node['x'] = element['lon']
+    node['osmid'] = element['id']
+
+    if 'tags' in element:
+        for useful_tag in useful_tags_node:
+            if useful_tag in element['tags']:
+                node[useful_tag] = element['tags'][useful_tag]
+    return node
+
+
+def parse_osm_nodes_paths(osm_data):
+    """
+    Construct dicts of nodes and paths with key=osmid and value=dict of
+    attributes.
+    Parameters
+    ----------
+    osm_data : dict
+        JSON response from from the Overpass API
+    Returns
+    -------
+    nodes, paths : tuple
+    """
+
+    nodes = {}
+    paths = {}
+    for element in osm_data['elements']:
+        if element['type'] == 'node':
+            key = element['id']
+            nodes[key] = get_node(element)
+        elif element['type'] == 'way':  # osm calls network paths 'ways'
+            key = element['id']
+            paths[key] = ox.get_path(element)
+
+    return nodes, paths
+
+
+def create_graph(response_jsons, name='unnamed', retain_all=True, bidirectional=False):
+    """
+    Create a networkx graph from Overpass API HTTP response objects.
+    Parameters
+    ----------
+    response_jsons : list
+        list of dicts of JSON responses from from the Overpass API
+    name : string
+        the name of the graph
+    retain_all : bool
+        if True, return the entire graph even if it is not connected
+    bidirectional : bool
+        if True, create bidirectional edges for one-way streets
+    Returns
+    -------
+    networkx multidigraph
+    """
+
+    # make sure we got data back from the server requests
+    elements = []
+
+    elements.extend(response_jsons['elements'])
+    if len(elements) < 1:
+        raise ox.EmptyOverpassResponse('There are no data elements in the response JSON objects')
+
+    # create the graph as a MultiDiGraph and set the original CRS to default_crs
+    G = nx.MultiDiGraph(name=name, crs=ox.settings.default_crs)
+
+    # extract nodes and paths from the downloaded osm data
+    nodes = {}
+    paths = {}
+
+    nodes_temp, paths_temp = parse_osm_nodes_paths(response_jsons)
+    for key, value in nodes_temp.items():
+        nodes[key] = value
+    for key, value in paths_temp.items():
+        paths[key] = value
+
+    # add each osm node to the graph
+    for node, data in nodes.items():
+        G.add_node(node, **data)
+
+    # add each osm way (aka, path) to the graph
+    G = ox.add_paths(G, paths, bidirectional=bidirectional)
+
+    # retain only the largest connected component, if caller did not
+    # set retain_all=True
+    if not retain_all:
+        G = ox.get_largest_component(G)
+
+    # add length (great circle distance between nodes) attribute to each edge to
+    # use as weight
+    if len(G.edges) > 0:
+        G = ox.add_edge_lengths(G)
+
+    return G
+
+punggol = gpd2.read_file('polygon-punggol.geojson')
+polygon = punggol['geometry'].iloc[0]
+
+
+busstop_Query = '[out:json];(node["highway"="bus_stop"](1.3891,103.8872,1.4222,103.9261);>;);out;'
+responsejson_Busstop = ox.overpass_request(data={'data': busstop_Query}, timeout=180)
+busstop_Graph = create_graph(responsejson_Busstop)
+busstop_Graph = ox.truncate_graph_polygon(busstop_Graph, polygon, truncate_by_edge=True,  retain_all=True)
+
+node, edge = ox.graph_to_gdfs(busstop_Graph)
+node.replace(' ', np.nan, inplace=True)
+node.to_csv("bus_stop.csv")
+# node.to_csv(node)
